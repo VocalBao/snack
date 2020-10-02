9d7423923e0951695ff25a3092f1406d741a0f4b
admin <admin@LAPTOP-MP192PL2>
2020-10-02 09:40:44 +0800

    拆分业务模块

---
+++ snack-config-server/src/main/java/com/bao/snack/config/App.java
@@ -0,0 +1,13 @@
+package com.bao.snack.config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
